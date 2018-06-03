8489cef6a19c2f71f3e55765faf843b0f90e359d
pappa <pappa@DESKTOP-MEDIA>
2018-06-03 13:10:22 +0200

    Add while in start

--- PiMongo/src/main/java/PiMongo/PiMongoStart.java
+++ PiMongo/src/main/java/PiMongo/PiMongoStart.java
@@ -6,5 +6,13 @@ public class PiMongoStart {
     public static void main( String[] args )
     {
         System.out.println( "Hello Mongo!" );
+        while(true) {
+        	System.out.println("hej");
+        	break;
+        }
+//        	try {
+//        	
+//        	}catch(ExitProgramException e){System.exit()}
+//        }
     }
 }
